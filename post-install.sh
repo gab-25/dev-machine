4399b514cc857af9bd17bfead908594a965a8272
Gabriele Sorci <71929275+gab-25@users.noreply.github.com>
2023-03-04 09:51:37 +0100

    add arch configuration and post install packages

---
+++ post-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#docker config
+sudo usermod -aG docker $USER
+
+#install yay
+cd /tmp
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git && echo Y | makepkg -si
+cd /tmp && rm -rf yay-git
+
+#customize kde-plasma
+echo Y | yay -S --nocleanmenu --nodiffmenu sierrabreeze-kwin-decoration-git
+
+#install google-chrome
+echo Y | yay -S --nocleanmenu --nodiffmenu google-chrome
+
+#install postman
+echo Y | yay -S --nocleanmenu --nodiffmenu postman-bin
+
+#install mongodb-compass
+echo Y | yay -S --nocleanmenu --nodiffmenu mongodb-compass
