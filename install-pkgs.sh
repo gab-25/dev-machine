23cf743a3e91e28556106ec8db28af5d8c7499ef
Gabriele Sorci <71929275+gab-25@users.noreply.github.com>
2023-03-11 09:56:35 +0000

    fix packages

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 #install packages
-sudo pacman -S --noconfirm docker git vim wget curl base-devel htop code dbeaver virtualbox gimp inkscape latte-dock libreoffice-still
+sudo pacman -S --noconfirm docker vim htop code dbeaver virtualbox gimp inkscape latte-dock libreoffice-still
 
 #docker config
 sudo usermod -aG docker $USER
-
-#install yay
-cd /tmp
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git && echo Y | makepkg -si
-cd /tmp && rm -rf yay-git
-
-#customize kde-plasma
-echo Y | yay -S --nocleanmenu --nodiffmenu sierrabreeze-kwin-decoration-git
+sudo systemctl enable docker
+sudo systemctl start docker
 
 #install google-chrome
 echo Y | yay -S --nocleanmenu --nodiffmenu google-chrome
